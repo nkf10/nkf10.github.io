3c2a12a49c8c1599233afa2209b4d56a10253e50
nkf10 <133275462+nkf10@users.noreply.github.com>
2023-11-12 17:36:24 -0500

    Update nola_iat_touch.js

--- nola_iat_touch.js
+++ nola_iat_touch.js
@@ -7,7 +7,7 @@ define(['pipAPI', 'https://nkf10.github.io/qstiat6.js'], function(APIConstructor
 		    name : 'ID', //Will appear in the data.
 		    title : {
 		      media : {word : 'Intellectual Disability'}, //Name of the category presented in the task.
-		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+		      css : {color:'#31b404','font-size':'1em'}, //Style of the category title.
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
 		    media : [ //Stimuli content as PIP's media objects
@@ -19,7 +19,7 @@ define(['pipAPI', 'https://nkf10.github.io/qstiat6.js'], function(APIConstructor
 		
 		    ],
 		    //Stimulus css (style)
-		    css : {color:'#31b404','font-size':'3em'}
+		    css : {color:'#31b404','font-size':'2em'}
 		  },	
 
   		attribute1 : 
@@ -27,7 +27,7 @@ define(['pipAPI', 'https://nkf10.github.io/qstiat6.js'], function(APIConstructor
 			name : 'N', //Attribute label
 			title : {
 				media : {word : 'Negative'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				css : {color:'#31b404','font-size':'1em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
@@ -39,14 +39,14 @@ define(['pipAPI', 'https://nkf10.github.io/qstiat6.js'], function(APIConstructor
 				
 			], 
 			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
+			css : {color:'#31b404','font-size':'2em'}
 			},
 		attribute2 : 
 			{
 			name : 'P', //Attribute label
 			title : {
 				media : {word : 'Positive'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				css : {color:'#31b404','font-size':'1em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
@@ -58,7 +58,7 @@ define(['pipAPI', 'https://nkf10.github.io/qstiat6.js'], function(APIConstructor
 				
 			], 
 			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
+			css : {color:'#31b404','font-size':'2em'}
 			},
 
   base_url : {//Where are your images at?
